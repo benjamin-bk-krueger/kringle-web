ec2a5de0ea222600340e73f4a5d068eb0392348a
Benjamin Krueger <konto@blk8.de>
2022-04-01 21:04:06 +0200

    updates

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
+import psycopg2 
+from psycopg2 import Error 
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/flask/hello')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    content = '<h1>Hello, World!</h1><br><br>\n'
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="postgres",
+                                    password="postgres",
+                                    host="kringle_database",
+                                    port="5432",
+                                    database="postgres")
+
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Executing a SQL query
+        cursor.execute("SELECT version();")
+        # Fetch result
+        record = cursor.fetchone()
+        content = content + "You are connected to - " + record + "<br>\n"
+
+        except (Exception, Error) as error:
+            return ("Error while connecting to PostgreSQL " + error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            content = content + "PostgreSQL connection is closed<br>\n"
+
+    return content
+
