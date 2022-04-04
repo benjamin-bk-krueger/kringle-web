0e6cc95e07eff6e3789ba5d4ef43383775558b34
Benjamin Krueger <konto@blk.pm>
2022-04-04 09:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -109,71 +109,28 @@ def get_all_junctions():
 
 @app.route('/api/world', methods=['POST'])
 def create_world():
-    record = json.loads(request.data)
-    with open('/tmp/world.json', 'w') as f:
-        f.write(json.dumps(record, indent=4))
-    return jsonify(record)
+    username = request.authorization["username"]
+    password = request.authorization["password"]
 
-
-'''
-    print(request.authorization["username"])
-    print(request.authorization["password"])
-'''
-
-'''
-@app.route('/', methods=['GET'])
-def query_records():
-    name = request.args.get('name')
-    print name
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for record in records:
-            if record['name'] == name:
-                return jsonify(record)
-        return jsonify({'error': 'data not found'})
-
-@app.route('/', methods=['PUT'])
-def create_record():
-    record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-    if not data:
-        records = [record]
+    if (username == "kringle" and password == "kringle"):
+        record = json.loads(request.data)
+        with open('/tmp/world.json', 'w') as f:
+            f.write(json.dumps(record, indent=4))
+        return jsonify({'success': 'world file stored'})
     else:
-        records = json.loads(data)
-        records.append(record)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(records, indent=2))
-    return jsonify(record)
+        return jsonify({'error': 'wrong credentials'})
 
-@app.route('/', methods=['POST'])
-def update_record():
-    record = json.loads(request.data)
-    new_records = []
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-    for r in records:
-        if r['name'] == record['name']:
-            r['email'] = record['email']
-        new_records.append(r)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
-    
-@app.route('/', methods=['DELETE'])
-def delte_record():
-    record = json.loads(request.data)
-    new_records = []
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for r in records:
-            if r['name'] == record['name']:
-                continue
-            new_records.append(r)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
-'''
+@app.route('/api/world', methods=['GET'])
+def get_world():
+    # name = request.args.get('name')
+    # print name
+    username = request.authorization["username"]
+    password = request.authorization["password"]
+
+    if (username == "kringle" and password == "kringle"):
+        with open('/tmp/world.json', 'r') as f:
+            data = f.read()
+            records = json.loads(data)
+            return jsonify(records)
+    else:
+        return jsonify({'error': 'wrong credentials'})
