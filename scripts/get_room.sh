da154e483ce914a55dc077493f1a3f1316d07619
Benjamin Krueger <konto@blk.pm>
2022-04-04 14:29:10 +0200

    better REST API

---
+++ scripts/get_room.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X GET -H "Authorization: Basic a3JpbmdsZTprcmluZ2xl" https://white.blk8.de/api/room/1
---
+++ scripts/set_room.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X POST -H "Content-Type: application/json" -d "{\"description\": \"You have arrived at the entry point to this year\u2019s KringleCon. In front of you you can see a green fence.\",\"name\": \"TheNorthPole_Orientation\"}" -H "Authorization: Basic a3JpbmdsZTprcmluZ2xl" https://white.blk8.de/api/room/1
