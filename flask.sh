788b40d7bd325970e3b153bde47b8d60e6d3e8bc
Benjamin Krueger <konto@blk8.de>
2022-06-02 23:30:34 +0200

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export FLASK_APP=app            # flask app to be started (app.py)
 
-cd /home/flask                  # go to our home directory
+cd /home/kringle                  # go to our home directory
 flask run -h 0.0.0.0 -p 5010    # listen to port 5010 on all interfaces
