3337dd339d5388e4e61c92f60ae8f84925a30264
Benjamin Krueger <konto@blk.pm>
2022-07-07 09:35:25 +0200

    Commenting... <3

--- static/helper_func.js
+++ static/helper_func.js
@@ -1,10 +1,14 @@
+/*
+    Get the content of an HTML element and copies it over to the clipboard.
+    Should be compatible to all currently used and up-to-date browsers.
+ */
 function CopyToClipboard(id) {
     let textToCopy = document.getElementById(id).innerText;
     if(navigator.clipboard) {
         navigator.clipboard.writeText(textToCopy).then(() => {
-            alert('Copied to clipboard')
+            alert('Value copied to clipboard')
         })
     } else {
-        console.log('Browser Not compatible')
+        console.log('Browser not compatible')
     }
 }
