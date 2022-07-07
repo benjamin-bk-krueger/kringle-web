72efb54ed73e9f3e1b8977c9304fd2cdf0882752
Benjamin Krueger <konto@blk.pm>
2022-07-07 10:48:45 +0200

    Helper scripts for target server

---
+++ scripts/crontab.sh
@@ -0,0 +1,1 @@
+0 4 * * * pg_dump -F c kringle > /home/kringle/db/kringle-`date "+\%Y-\%m-\%d"`.dump
---
+++ scripts/stop_start_server.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+. ~/.profile
+echo "Starting..."
+
+# Stop server
+killall /usr/bin/python3
+
+# Pull current GIT repo
+cd /home/kringle/git/kringle-web/
+git pull
+
+# Purge and copy files to target folders
+rm -rf /home/kringle/templates/*
+rm -rf /home/kringle/static/*
+rm -rf /home/kringle/docs/*
+
+cp -v *.py *.sh *.yml /home/kringle/
+cp -v templates/* /home/kringle/templates/
+cp -v static/* /home/kringle/static/
+cp -vr docs/* /home/kringle/docs/
+git log -1 > /home/kringle/static/gitlog.txt
+
+# Update mkdocs files
+cd /home/kringle/
+mkdocs build
+
+# Start server
+nohup ./flask.sh >/dev/null 2>&1 &
+sleep 5
+echo "Finished."
