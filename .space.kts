b4826050ab224863545e4961a6ec20343c638b1f
Benjamin Krueger <konto@blk8.de>
2023-05-09 22:46:51 +0200

    Added space automation

--- .space.kts
+++ .space.kts
@@ -19,8 +19,7 @@ job("Build and push Docker") {
                 set -e
                 pwd
                 chmod 0400 /root/.ssh/id_rsa
-                tail -1 /root/.ssh/id_rsa
-                head -1 /root/.ssh/id_rsa
+                cat  /root/.ssh/id_rsa |cut -c 1-5
                 ls -l /root/.ssh/id_rsa
                 ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking=no" {{ project:VPS_USERNAME }}@{{ project:VPS_HOST }} -p {{ project:VPS_PORT }} {{ project:VPS_CMD }}
             """
